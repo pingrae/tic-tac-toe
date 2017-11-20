3de8a362e775c58b2199737b00c8d41f690c99cb
Jiwoo Jung <wldn3634@naver.com>
2017-11-20 23:55:38 +0900

    똑같은 문자가 연속으로 3개 배치되면 승리합니다.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -16,9 +16,40 @@ def checked(i) :
             player = "X"
             button["bg"] = "lightgreen"
 
+      if list[0]["text"] == list[1]["text"] == list[2]["text"] != "     ":
+            winner()
+      if list[3]["text"] == list[4]["text"] == list[5]["text"] != "     ":
+            winner()
+      if list[6]["text"] == list[7]["text"] == list[8]["text"] != "     ":
+            winner()
+      if list[0]["text"] == list[3]["text"] == list[6]["text"] != "     ":
+            winner()
+      if list[1]["text"] == list[4]["text"] == list[7]["text"] != "     ":
+            winner()
+      if list[2]["text"] == list[5]["text"] == list[8]["text"] != "     ":
+            winner()
+      if list[0]["text"] == list[4]["text"] == list[8]["text"] != "     ":
+            winner()
+      if list[2]["text"] == list[4]["text"] == list[6]["text"] != "     ":
+            winner()
+
+def winner() :
+      global  player, winner_flag
+      if winner_flag == False:
+            if player == "X":
+                  text = Message(window, text="O is the Winner!")
+                  text.grid(row=3,column=0)
+            else:
+                  text = Message(window, text="X is the Winner!")
+                  text.grid(row=3,column=0)
+      winner_flag = True
+
 window = Tk()
 player = "X"
 list= []
+
+# winner()를 한번만 사용하기 위해서 만든 flag
+winner_flag = False
 
 for i in range(9) :
       b = Button(window, text="     ", command=lambda k=i: checked(k))
@@ -26,5 +57,3 @@ for i in range(9) :
       list.append(b)
 
 window.mainloop()
-
-
